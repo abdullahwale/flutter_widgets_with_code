2361538ccca480f47a0eaed678c0a2c7a7c72848
abdullahwale <abdullahwale@gmail.com>
2021-03-09 08:56:43 +0500

    Step03
    21. Updating Widget Data (Or Using StatelessWidget Incorrectly)

--- basic_quiz_app/lib/main.dart
+++ basic_quiz_app/lib/main.dart
@@ -7,15 +7,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer chosen!');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
-      'What\'s your favorite animarl?',
+      'What\'s your favorite animal?',
     ];
     return MaterialApp(
       home: Scaffold(
