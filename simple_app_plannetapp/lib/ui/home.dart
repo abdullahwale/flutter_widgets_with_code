87683a4ab006d5d1a762f6db2e6c4d90a821bfab
abdullahwale <abdullahwale@gmail.com>
2021-03-10 11:19:42 +0500

    S06 Add Container for TextFiled
    Container(
                  margin: const EdgeInsets.all(3.0),
                  alignment: Alignment.center,
                  child: Column(
                    children: [
                      TextField(
                        controller: null,
                        keyboardType: TextInputType.number,
                        decoration: InputDecoration(
                            labelText: 'What is your Weight on Earth',
                            hintText: 'In Ponds',
                            icon: Icon(Icons.person_outline)),
                      )
                    ],
                  ),
                ),

--- simple_app_plannetapp/lib/ui/home.dart
+++ simple_app_plannetapp/lib/ui/home.dart
@@ -8,33 +8,44 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-
-
-
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:  AppBar(
+    return Scaffold(
+      appBar: AppBar(
         title: new Text("Weight On Planet X"),
         centerTitle: true,
         backgroundColor: Colors.black38,
       ),
       backgroundColor: Colors.blueGrey,
-      body:  Container(
+      body: Container(
         alignment: Alignment.center,
         child: ListView(
           padding: const EdgeInsets.all(2.5),
           children: [
-            Image.asset('images/planet.png',
-            height: 133.0,
-            width: 280.0,),
+            Image.asset(
+              'images/planet.png',
+              height: 133.0,
+              width: 280.0,
+            ),
+            Container(
+              margin: const EdgeInsets.all(3.0),
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: null,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'What is your Weight on Earth',
+                        hintText: 'In Ponds',
+                        icon: Icon(Icons.person_outline)),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
-
-      );
+      ),
     );
   }
-
-
 }
