2e2712a4e8db70973c76a87fc306561fb355e353
abdullahwale <abdullahwale@gmail.com>
2021-03-25 10:00:07 +0500

    this main file of tip calculator

---
+++ tipcalculator_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'tip.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tip Calculator',
+      theme: new ThemeData(primaryColor: Colors.black, fontFamily: "Satisfy"),
+      home: Tip(),
+    );
+  }
+}
