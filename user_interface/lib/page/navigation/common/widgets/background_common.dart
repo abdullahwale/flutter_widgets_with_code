aa68fe776b3bc315d4996d32e87171d6a92f7ef2
abdullahwale <abdullahwale@gmail.com>
2021-03-29 12:28:00 +0500

    User Interface Examples

---
+++ user_interface/lib/page/navigation/common/widgets/background_common.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:user_interface/const/color_const.dart';
+
+class BackgroundCommon extends StatelessWidget {
+  BackgroundCommon({this.child, Key key}) : super(key: key);
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [YELLOW, GREEN, BLUE],
+      )),
+      child: child);
+}
