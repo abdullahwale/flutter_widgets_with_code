4bd8023433550a0d27068ba8157796372c82845b
abdullahwale <abdullahwale@gmail.com>
2021-03-30 13:09:50 +0500

    Add Data for Each Tabs

--- tabs_demo/lib/main.dart
+++ tabs_demo/lib/main.dart
@@ -21,6 +21,13 @@ class TabBarDemo extends StatelessWidget {
             ),
             title: Text('Tabs Demo'),
           ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
         ),
       ),
     );
