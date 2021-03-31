e69f2acad985be0d893fb56ff1f852415ab1bfca
abdullahwale <abdullahwale@gmail.com>
2021-03-31 17:30:29 +0500

    Step 05: Adding FlOATING ACTION BUTTON AND THIER METHOD clickable

--- todoapp_commit_by_commit/lib/ui/todoScreen.dart
+++ todoapp_commit_by_commit/lib/ui/todoScreen.dart
@@ -8,6 +8,20 @@ class todoScreen extends StatefulWidget {
 class _todoScreenState extends State<todoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: Column(),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: "Add Item",
+          backgroundColor: Colors.redAccent,
+          child: new ListTile(
+            title: new Icon(Icons.add),
+          ),
+          onPressed: _showFormDialog),
+    );
+  }
+
+  void _showFormDialog() {
+    print("FLoating Action button");
   }
 }
