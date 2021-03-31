a07903478684bbaf6ddd3e3d5a0af94112773871
abdullahwale <abdullahwale@gmail.com>
2021-03-31 17:19:08 +0500

    just add project and add pakages

---
+++ mak_detection/android/app/src/main/java/com/example/mak_detection/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.mak_detection;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ todoapp_commit_by_commit/android/app/src/main/java/com/example/todoapp_commit_by_commit/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.todoapp_commit_by_commit;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
