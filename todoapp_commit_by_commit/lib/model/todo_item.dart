f907a6c6cc7c678a89712b349c9b2481357196e0
abdullahwale <abdullahwale@gmail.com>
2021-03-31 17:36:13 +0500

    Step 06: Adding todo_item.dart and thier data members with constructor

---
+++ todoapp_commit_by_commit/lib/model/todo_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class todo_item extends StatelessWidget {
+  String _itemName;
+  String _dateCreated;
+  int _id;
+  todo_item(this._itemName, this._dateCreated);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
