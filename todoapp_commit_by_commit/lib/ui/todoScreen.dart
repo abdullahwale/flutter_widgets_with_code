ecf233739f76338a9d3b9cf934a4fd182a98eea2
abdullahwale <abdullahwale@gmail.com>
2021-04-01 06:23:05 +0500

    Step 11 Display data in list view

--- todoapp_commit_by_commit/lib/ui/todoScreen.dart
+++ todoapp_commit_by_commit/lib/ui/todoScreen.dart
@@ -11,7 +11,7 @@ class _todoScreenState extends State<todoScreen> {
   final TextEditingController _textEditingController =
       new TextEditingController();
   var db = new DatabaseHelper();
-
+  final List<todo_item> _itemList = <todo_item>[];
   @override
   void initState() {
     // TODO: implement initState
@@ -23,7 +23,35 @@ class _todoScreenState extends State<todoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(),
+      body: Column(
+        children: [
+          Flexible(
+              child: ListView.builder(
+                  padding: EdgeInsets.all(0.0),
+                  reverse: false,
+                  itemCount: _itemList.length,
+                  itemBuilder: (_, int index) {
+                    return Card(
+                      color: Colors.white10,
+                      child: ListTile(
+                        title: _itemList[index],
+                        onLongPress: () => debugPrint(""),
+                        trailing: Listener(
+                          key: Key(_itemList[index].itemName),
+                          child: Icon(
+                            Icons.remove_circle,
+                            color: Colors.redAccent,
+                          ),
+                          onPointerDown: (pointEvent) => debugPrint(""),
+                        ),
+                      ),
+                    );
+                  })),
+          Divider(
+            height: 1.0,
+          )
+        ],
+      ),
       floatingActionButton: new FloatingActionButton(
           tooltip: "Add Item",
           backgroundColor: Colors.redAccent,
@@ -74,17 +102,21 @@ class _todoScreenState extends State<todoScreen> {
     _textEditingController.clear();
     todo_item item = todo_item(text, DateTime.now().toIso8601String());
     int savedItem = await db.saveItem(item);
+    todo_item addedItem = await db.getItem(savedItem);
+    setState(() {
+      _itemList.insert(0, addedItem);
+    });
     print(savedItem);
   }
 
   void _readNoDoList() async {
     List items = await db.getItems();
     items.forEach((item) {
-      todo_item noDoItem = todo_item.fromMap(item);
-      // setState(() {
-      //   _itemList.add(todo_item.map(item));
-      // });
-      print("Db items: ${noDoItem.itemName}");
+      //todo_item noDoItem = todo_item.fromMap(item);
+      setState(() {
+        _itemList.add(todo_item.map(item));
+      });
+      //print("Db items: ${noDoItem.itemName}");
     });
   }
 }
