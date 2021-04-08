36011dec5094b09097260143a0542ee391438e1c
abdullahwale <abdullahwale@gmail.com>
2021-04-08 17:23:10 +0500

    best logic luddo

---
+++ luddo_game_logic/android/app/src/main/java/com/example/luddo_game_logic/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.luddo_game_logic;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ludo_logic_best/android/app/src/main/java/com/example/ludo_logic_best/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ludo_logic_best;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
